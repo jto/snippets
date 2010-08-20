14c31ef908fef12521db54c8583e7764274c4878
jto <boudhevil@gmail.com>
2010-08-20 11:19:56 +0200

    ^^

--- app/models/Snippet.java
+++ app/models/Snippet.java
@@ -1,7 +1,9 @@
 package models;
 
 import play.*;
+import play.libs.*;
 import play.db.jpa.*;
+import play.cache.*;
 import play.data.validation.*;
 
 import javax.persistence.*;
@@ -21,11 +23,26 @@ public class Snippet extends Model{
   @Required
   @Column(length=20)
   public String ident;
-  @Column(length=200)
-  public String description;
   public int score = 0;
 
   public static boolean exists(String ident){
     return find("ident=?", ident).first() != null;
+  }
+  
+  public String getDescription(){
+    String url = String.format("http://gist.github.com/api/v1/json/%s", ident);
+    String desc = Cache.get(ident, String.class);
+    if(desc == null){
+      try{
+        desc = WS.url(url).get().getJson().getAsJsonObject()
+          .get("gists").getAsJsonArray().get(0).getAsJsonObject()
+          .get("description").getAsString();
+      }
+      catch(Exception e){
+        desc = "";
+      }
+      Cache.set(this.ident, desc);
+    }
+    return desc;
   }
 }
